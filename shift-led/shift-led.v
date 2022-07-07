16328a269c8b305b1a612b65eded3a26f87c74f9
Apollo Heidal <apolloheidal@gmail.com>
2022-07-06 23:13:19 -0700

    working shift led in one always block

--- shift-led/shift-led.v
+++ shift-led/shift-led.v
@@ -1,48 +1,38 @@
 // Shift Tang Nano RGB LED through all possible states 
 
-module shift_led #(
-    parameter   SHIFT_HZ        = 1,
-                TANG_NANO_HZ    = 24_000_000 // 24Mhz
-) (
-    input               clk,
-                        rst_button,
+module shift_led 
+#(
+    parameter  SHIFT_HZ     = 2,
+    parameter  TANG_NANO_HZ = 24_000_000
+    ) 
+    (
+    input               sys_clk,
+    input               sys_rst_n,
 
     output reg [2:0]    led
 );
-
-    localparam  COUNT_MAX   = (TANG_NANO_HZ / SHIFT_HZ) - 1,
-                COUNT_SIZE  = $clog2(COUNT_MAX);
-
-    wire rst, shift;
+    localparam  COUNT_MAX   = TANG_NANO_HZ / SHIFT_HZ;
+    localparam  COUNT_SIZE  = $clog2(COUNT_MAX);
 
     // divided clock counter
-    reg     [COUNT_SIZE:0]  count       = 0;
-    wire    [COUNT_SIZE:0]  count_inc   = count + 1;
-    always @ (posedge clk) begin
-        if (count == COUNT_MAX) begin
-            count <= 0;
-            shift <= 1;
+    reg [23:0] count; 
+
+    wire    rst     = !sys_rst_n;
+    wire    shift   = (count == COUNT_MAX);
+
+    always @ (posedge rst or posedge sys_clk) begin
+        if (rst) begin// non-zero evaluates as true 
+            count   <= 24'd0;
+            led     <= 'b0;
+        end else if (shift) begin
+            if (led == 3'b110) begin
+                led <= 3'b0;
+            end else begin
+                led <= led + 3'b1;
+            end
+            count <= 24'd0;
         end else begin
-            count <= count_inc;
+            count <= count + 1;
         end
     end
-
-    // RGB LED
-    wire [2:0] led_ctrl;
-    always @(posedge rst or posedge shift) begin
-        if (rst) begin
-            led <= 0;
-            count <= 0;
-        end else if (led == 3'b111) begin
-            led <= 0;
-        end else begin
-            led <= led + 1;            
-        end
-    end
-
-    // logic
-    assign led = led_ctrl;
-    assign rst = !rst_button;
-
-
 endmodule
